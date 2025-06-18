1150c7b67658b5f8aff08fc6aac5d4758c2e7c0b
Marcus Schou <schou.business@gmail.com>
2025-06-19 00:55:37 +0200

    Add files via upload

---
+++ ml_project_generator/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ml-project-generator',
+    version='0.1.0',
+    packages=['project_generator'],
+    entry_points={
+        'console_scripts': [
+            'ml-project = ml_project_generator.__main__:main',
+        ],
+    },
+    python_requires='>=3.6',
+    author='Marcus Schou',
+    description='A simple CLI tool to generate ml project folder templates.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
